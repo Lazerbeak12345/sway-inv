3fe1bdfa90d4042152435ee6ff34ea383908381d
Nathan Fritzler <nfblaster@live.com>
2023-04-19 22:47:53 -0600

    feat(init): Also disable i3 if present

--- init.lua
+++ init.lua
@@ -13,17 +13,19 @@ print[[
 ]]
 -- See README.md for art credit
 
-
+local minetest = minetest
 do
-	-- Before we do anything, disable sfinv and unified_inventory. (code from i3 at
-	--  commit bd5ea4e6). Also under MIT, under Jean-Patrick-Guerrero and
-	--  contributors. Modified to fit my style guidelines.
+	-- Before we do anything, disable sfinv, unified_inventory, and i3. (code from i3 at commit bd5ea4e6). Also under MIT,
+	--  under Jean-Patrick-Guerrero and contributors. Modified to fit my style guidelines, and to also disable i3
 	if minetest.global_exists("sfinv") then
-		function sfinv.set_player_inventory_formspec() return end
+		function sfinv.set_player_inventory_formspec() end
 		sfinv.enabled = false
 	end
 	if minetest.global_exists("unified_inventory") then
-		function unified_inventory.set_inventory_formspec() return end
+		function unified_inventory.set_inventory_formspec() end
+	end
+	if minetest.global_exists("i3") then
+		function i3.set_fs() end
 	end
 end
 
