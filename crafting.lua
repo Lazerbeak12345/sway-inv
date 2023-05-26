2b27b8cf5e3c4b0fec3431be9bb1f81e53856041
Nathan Fritzler <nfblaster@live.com>
2023-05-25 22:13:26 -0600

    docs[sway:crafting]: note my thoughts about future api

--- crafting.lua
+++ crafting.lua
@@ -8,6 +8,13 @@ local gui = sway.widgets
 sway.register_page("sway:crafting", {
 	title = S("Crafting"),
 	-- Replace this function to modify crafting page
+	-- I'll be honest... this isn't a perfect solution. I've decided to knowingly compromise so _something_ happens,
+	--  rather than let the project fail. Now is the perfect time to finish this project. The time could literally not be
+	--  better than this.
+	-- The alternative idea is to make a "template" middle layer between sway (and and everything that uses it) and flow.
+	--  This layer would not immidietly expand out to final form, but delay all function calls that would do so until right
+	--  before getting handed off to flow. Thus modders would have full sexp control over the gui before the expansion
+	--  phase (perhaps with the exception of callbacks?). Other ideas were explored but were far too bad to work.
 	filter = function (_, _, _, elm) return elm end,
 	get = function(self, player, context)
 		return gui.sway.Form{
